7db3dcbf4f270cdebbca201735d53ef40b6220ea
Marcelo Luiz Jurevicius do Nascimento <marcelo.nascimento@livelo.com.br>
2019-11-10 11:19:23 -0300

    initial comit

---
+++ Controllers/HoraCertaController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace app_hora_certa.Controllers
+{
+    [Route("v1/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public ActionResult<string> Get()
+        {
+            return DateTime.Now.ToLongDateString();
+        }
+    }
+}
